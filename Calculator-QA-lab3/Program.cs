ec382fe185ffe9408a652aa3d9a47ea83c7ae073
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-06-09 20:22:31 -0400

    First test set up

---
+++ Calculator-QA-lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_QA_lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lab3-Test/TriangleTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_QA_lab3;
+using NUnit.Framework;
+
+namespace Lab3_Test
+{
+    public class TriangleTest1
+    {
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            //Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "The triangle is valid.";
+
+            //Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
